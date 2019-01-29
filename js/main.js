eee7fb19e5ef50fe0f4a52e6e26fef68c4e0dbf2
SJ <JBenn1980@gmail.com>
2019-01-29 11:16:17 -0500

    File Cleanup & Consolidation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
 (function($) {
-
 	"use strict";	
 
-	$('.carousel').carousel({
+	var	$window = $(window),
+		$body = $('body'),
+		$nav = $('#nav');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('#nav a, .scrolly').scrolly({
+			speed: 1000,
+			offset: function() { return $nav.height(); }
+		});
+		
+			$('.carousel').carousel({
 		interval: false
 	});
 
@@ -32,8 +56,5 @@
 		$('html, body').animate({scrollTop : 0},800);
 		return false;
 	});
-
- 
-	
 
 })(jQuery);
